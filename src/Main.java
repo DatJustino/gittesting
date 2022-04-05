7e514c04dc83f7dfbb24a2b65a5ea7b0f32e4b3f
DatJustino <100353161+DatJustino@users.noreply.github.com>
2022-04-05 14:37:12 +0200

    main is renewed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    Dog doggo = new Dog();
-    Cat catto = new Cat();
+    Dog doggo = new Dog("doggo");
+    Cat catto = new Cat("catto");
     doggo.makeSound();
     catto.makeSound();
 
